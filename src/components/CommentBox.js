d1818b4e67e0d4337dced38e57c39ebeeefda4a5
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-07 16:35:42 -0200

    feat: Adding new list and box component

---
+++ src/components/CommentBox.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class CommentBox extends Component {
+  render() { 
+    return ( <div> This is a comment box! </div> );
+  }
+}
+ 
+export default CommentBox;
