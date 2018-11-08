aef1a79a4664d0362346975f75ea0c6a9c9acff7
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-07 23:46:36 -0200

    feat: Comment box test w/ mount

---
+++ src/components/__test__/CommentBox.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let component;
+
+beforeEach(() => {
+  component = mount(<CommentBox/>);
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(component.find('textarea').length).toEqual(1);
+  expect(component.find('button').length).toEqual(1);
+});
