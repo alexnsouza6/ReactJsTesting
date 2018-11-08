1b4f9e364411b30a4b0e89f001eb5e50c0a3d92b
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-07 23:24:07 -0200

    feat: enabling absolute paths

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let component;
+beforeEach(() => {
+  component = shallow(<App/>);
+});
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
+  expect(component.find(CommentBox).length).toEqual(1);
+});
 
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('This is a comment box!');
-  ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list', () => {
+  expect(component.find(CommentList).length).toEqual(1);
 });
