a118d3eb170e952eb72e4eaaf0c7a4a93e073d49
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-14 16:23:42 -0200

    feat: Adding list for comments

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class CommentList extends Component {
+  static propTypes = {
+    comments: PropTypes.node.isRequired,
+  };
+
   state = {};
 
+  renderComments = () => {
+    const { comments } = this.props;
+    comments.map(comment => <li key={comment}>{comment.title}</li>);
+  };
+
   render() {
-    return <div> This is a comment list! </div>;
+    return (
+      <div>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
   }
 }
 
-export default CommentList;
+const mapStateToProps = state => ({ comments: state.comments });
+
+export default connect(mapStateToProps)(CommentList);
