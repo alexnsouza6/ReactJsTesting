e697acf59369a667886d184123078eb6ab5a8e86
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-17 18:43:42 -0200

    feat: creating header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
+import Header from './Header';
 
 export default () => (
   <Fragment>
+    <Header />
     <Route path="/post" component={CommentBox} />
     <Route path="/" component={CommentList} />
   </Fragment>
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { changeAuth } from './actions/index';
+
+class Header extends Component {
+  renderButton = ({ auth, changeAuth }) => {
+    if (auth) {
+      return (
+        <button type="submit" onClick={() => changeAuth(false)}>
+          Sign Out
+        </button>
+      );
+    }
+    return (
+      <button type="submit" onClick={() => changeAuth(true)}>
+        Sign In
+      </button>
+    );
+  };
+
+  render() {
+    return (
+      <ul>
+        <li>
+          <Link to="/"> Home </Link>
+        </li>
+        <li>
+          <Link to="/post"> Post a Comment </Link>
+        </li>
+        <li>{this.renderButton(this.props)}</li>
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(
+  mapStateToProps,
+  { changeAuth },
+)(Header);
