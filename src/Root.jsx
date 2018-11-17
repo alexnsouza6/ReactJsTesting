0d4e44b4c13cfa70e14643e421b8e704ad973076
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-17 15:30:16 -0200

    feat: Adding react router

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import reduxPromise from 'redux-promise';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './components/reducers';
 
 const Root = ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    reducers,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxPromise)),
+  );
+
   return <Provider store={store}>{children}</Provider>;
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Root from './Root';
 
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
   </Root>,
   document.querySelector('#root'),
 );
