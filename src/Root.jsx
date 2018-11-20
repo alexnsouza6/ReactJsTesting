6ed9ab57b5823998a5d251ccfbe618317dddfd96
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-19 23:11:08 -0200

    feat: adding custom middleware to store

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,11 +4,16 @@ import { Provider } from 'react-redux';
 import reduxPromise from 'redux-promise';
 import { createStore, applyMiddleware, compose } from 'redux';
 import async from './middlewares/async';
+import stateValidator from './middlewares/stateValidator';
 
 const Root = ({ children, initialState = {} }) => {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  const store = createStore(async, initialState, composeEnhancers(applyMiddleware(reduxPromise)));
+  const store = createStore(
+    async,
+    initialState,
+    composeEnhancers(applyMiddleware(async, stateValidator)),
+  );
 
   return <Provider store={store}>{children}</Provider>;
 };
